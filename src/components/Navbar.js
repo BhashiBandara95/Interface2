4275d14f8020b72b97d7057eada7749e7df0374d
bhasitha95 <amandabhashitha95@gmail.com>
2022-10-04 21:55:58 +0530

    Edited code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import "./css/navbar.css"
 
-export default function Navbar({renderSignin},{renderSignup}){
+export default function Navbar({renderSignin,renderSignup}){
     
 
     return (
         <div className="navbar">
             <a className="active" href="/"><i className="fa fa-fw fa-home"></i> Home</a>
-            <a href="#" onClick={renderSignin}><i className="fa fa-fw fa-envelope"></i> Signin</a>
-            <a href="/"><i className="fa fa-fw fa-user"></i> signup</a>
+            <span onClick={renderSignin}><a href="#" ><i className="fa fa-fw fa-envelope"></i> Signin</a></span>
+            <span><a href="#"><i className="fa fa-fw fa-user" onClick={renderSignup}></i> Signup</a></span>
+            
         </div>
     )
 
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,64 +1,79 @@
-import './signup.css';
+import './login.css';
+import profile from "./a.png"
+import user from "./user.png"
+import nic from "./identity-card.png"
+import post from "./post.png"
+import contact from "./contact.png"
+import email from "./email.png"
+import password from "./reset-password.png"
 
 
-import { useState } from 'react';
-function Signin(){
-    const [UserName,setUserName] = useState("")
-    const [UserNIC,setUserNIC] = useState("")
-    const [UserPost,setUserPost] = useState("")
-    const [UserUserContact,setUserContact] = useState("")
-    const [Useremail,setUseremail] = useState("")
-   const [password,setPassword] = useState("")
 
-    const submit = (e)=>{
-        e.preventDefault()
-        console.log({UserName,UserNIC,UserPost,UserContact,Useremail,password});
+
+export default function Signup() {
+
+  const myFn = async(event)=>{
+    event.preventDefault()
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    // console.log(JSON.parse);
+    const res = await fetch('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-anjli/endpoint/client/add',{
+      method:"POST",
+      headers:{
+        'Content-Type':"application/json"
+      },
+      body:JSON.stringify(data)
+    })
+    if (res.status === 200) {
+     
     }
+    else {
+      
+    }
+  }
 
-function App(){
   return (
-    <div className="main">
-     <div className="sub-main">
+    <form className="main" onSubmit={myFn}>
+         <div className="sub-main">
        <div>
          <div className="imgs">
            <div className="container-image">
-             
-
+             <img src={profile} alt="profile" className="profile"/>
            </div>
-
-
          </div>
          <div>
-           <h1>Signin Page</h1>
+           <h1>Signup Page</h1>
            <div>
-             <input type="text" placeholder="UserName" className="name"/>
+           <img src={user} alt="user" className="email"/>
+             <input type="text" placeholder="user name" className="name" name='user'/>
            </div>
-
-           <div className="second-input">
-             <input type="UserNIC" placeholder="UserNIC" className="number"/>
+           <div >
+           <img src={nic} alt="nic" className="email"/>
+             <input type="text" placeholder="user NIC" className="name" name='nic' />
+           </div> 
+           <div >
+           <img src={post} alt="post" className="email"/>
+             <input type="text" placeholder="user post" className="name" name='post'/>
            </div>
-
-           <div className="third-input">
-             <input type="UserPost" placeholder="UserPost" className="name"/>
+           <div >
+           <img src={contact} alt="contact" className="email"/>
+             <input type="text" placeholder="user contact" className="name" name='contact'/>
            </div>
-
-          
-
-           <div className="fifth-input">
-             <input type="UserContact" placeholder="UserContact" className="number"/>
+           <div >
+           <img src={email} alt="email" className="email"/>
+             <input type="text" placeholder="user email" className="name" name='email'/>
            </div>
-
-           <div className="seventh-input">
-             <input type="Useremail" placeholder= "Useremail" className="name>
-          </div>
-
-          </div>
-
-          <div className="Signup-button">
-          
-          <button onClick={submit}>Signup </button>
+           <div >
+           <img src={password} alt="password" className="email"/>
+             <input type="password" placeholder="user pasword" className="name" name='password'/>
+           </div>
+          <div className="login-button">
+          <button>Signup</button>
           </div>
            
+            <p className="link">
+              <a href="/">Forgot password ?</a> Or<a href="/">Sign Up</a>
+            </p>
            
  
          </div>
@@ -66,10 +81,6 @@ function App(){
        
 
      </div>
-     </div>
-   
-  );
+    </form>
+  )
 }
-
-export default Signup;
-   
